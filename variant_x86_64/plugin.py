cd26f332cd70fdfbccbc7850f1cd2e236a268faf
Michał Górny <mgorny@gentoo.org>
2025-04-10 17:55:48 +0200

    Use archspec to detect supported architecture level

--- variant_x86_64/plugin.py
+++ variant_x86_64/plugin.py
@@ -1,16 +1,42 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import archspec.cpu
+
 from variantlib.base import PluginType
 from variantlib.base import VariantFeatureConfigType
 from variantlib.models.provider import VariantFeatureConfig
 
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
 
 class Plugin(PluginType):
     namespace = "x86_64"
+    max_known_level = 4
+
+    @staticmethod
+    def _level_range(max_level: int) -> Generator[str]:
+        for level in range(max_level, 0, -1):
+            yield f"v{level}"
 
     def get_all_configs(self) -> list[VariantFeatureConfigType]:
         return [
-            VariantFeatureConfig("level", ["v4", "v3", "v2", "v1"]),
+            VariantFeatureConfig(
+                "level", list(self._level_range(self.max_known_level))
+            ),
         ]
 
     def get_supported_configs(self) -> list[VariantFeatureConfigType]:
-        # TODO: do CPU detection, fitler
-        return self.get_all_configs()
+        microarch = archspec.cpu.host().generic
+        if microarch.name.startswith("x86_64_v"):
+            supported_level = int(microarch.name.removeprefix("x86_64_v"))
+            return [
+                VariantFeatureConfig(
+                    "level",
+                    list(self._level_range(min(supported_level, self.max_known_level))),
+                ),
+            ]
+
+        return []
