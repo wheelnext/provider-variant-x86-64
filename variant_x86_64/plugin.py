6fb3e55cea157c1a4f12d8df9a1424184a96f9cd
Michał Górny <mgorny@gentoo.org>
2025-04-17 17:28:37 +0200

    Add support for SIMD features

--- variant_x86_64/plugin.py
+++ variant_x86_64/plugin.py
@@ -15,7 +15,84 @@ if TYPE_CHECKING:
 
 class Plugin(PluginType):
     namespace = "x86_64"
+
     max_known_level = 4
+    """Max microarchitecture level supported at the time"""
+
+    all_features = [
+        # zen5 features features
+        "avx_vnni",
+        "cppc",
+        "ibrs_enhanced",
+        "tsc_adjust",
+        # zen4 features features
+        "flush_l1d",
+        # sapphirerapids features
+        "movdir64b",
+        "movdiri",
+        # icelake features
+        "avx512_bf16",
+        "avx512_bitalg",
+        "avx512_vbmi2",
+        "avx512_vnni",
+        "avx512_vp2intersect",
+        "avx512_vpopcntdq",
+        "avx512ifma",
+        "avx512vbmi",
+        "rdpid",
+        "sha_ni",
+        "vaes",
+        "vpclmulqdq",
+        # skylake_avx512 features
+        "clwb",
+        "clzero",
+        # x86-64-v4 features
+        "avx512bw",
+        "avx512cd",
+        "avx512dq",
+        "avx512f",
+        "avx512vl",
+        # skylake features
+        "clflushopt",
+        "gfni",
+        "rdseed",
+        "xsavec",
+        "xsaveopt",
+        # broadwell features
+        "adx",
+        # x86-64-v3 features
+        "avx2",
+        "avx",
+        "bmi2",
+        "bmi1",
+        "abm",
+        "f16c",
+        "fma",
+        "movbe",
+        "xsave",
+        # sandybridge features
+        "rdrand",
+        # westmere features
+        "aes",
+        # nehalem features
+        "pclmulqdq",
+        # steamroller features
+        "sse4a",
+        "fsgsbase",
+        # x86-64-v2 features
+        "sse4_2",
+        "sse4_1",
+        "ssse3",
+        "sse3",
+        "cx16",
+        "lahf_lm",
+        "popcnt",
+        # x86-64-v1 features
+        "sse2",
+        "sse",
+        "mmx",
+    ]
+    """All features supported by archspec at the time, sorted in preference order"""
 
     @staticmethod
     def _level_range(max_level: int) -> Generator[str]:
@@ -27,17 +104,22 @@ class Plugin(PluginType):
             VariantFeatureConfig(
                 "level", list(self._level_range(self.max_known_level))
             ),
-        ]
+        ] + [VariantFeatureConfig(feature, ["on"]) for feature in self.all_features]
 
     def get_supported_configs(self) -> list[VariantFeatureConfigType]:
-        microarch = archspec.cpu.host().generic
-        if microarch.name.startswith("x86_64_v"):
-            supported_level = int(microarch.name.removeprefix("x86_64_v"))
+        microarch = archspec.cpu.host()
+        generic = microarch.generic
+        if generic.name.startswith("x86_64_v"):
+            supported_level = int(generic.name.removeprefix("x86_64_v"))
             return [
                 VariantFeatureConfig(
                     "level",
                     list(self._level_range(min(supported_level, self.max_known_level))),
                 ),
+            ] + [
+                VariantFeatureConfig(feature, ["on"])
+                for feature in self.all_features
+                if feature in microarch
             ]
 
         return []
